fdc13f5d2c70e7312464f26517447d239085a23e
Dagim <dagimamo6@gmail.com>
2025-03-17 04:17:21 -0700

    Implemented a responsive homepage with multiple sections: - Trending Books, Favorites, Top Rated, New, and Free. - Created a dashboard with bottom navigation, app bar, and drawer. - Added library and explore pages as placeholders.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:smart_ebook/presentation/screens/dashboard_page.dart';
 
+
 void main() {
   runApp(MyApp());
 }
--- lib/presentation/screens/drawerpage.dart
+++ lib/presentation/screens/drawerpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'drawerComponent/library.dart';
 import 'drawerComponent/logout.dart';
 import 'drawerComponent/paymentMethods.dart';
